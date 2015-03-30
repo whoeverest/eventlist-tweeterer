4d4225ba642ca521d79d7db5c921ba4fb7796f2b
Andrej T <andrejtrajchevski@gmail.com>
2015-03-30 02:11:32 +0200

    switch to spaces

--- tweet-event.py
+++ tweet-event.py
@@ -7,19 +7,19 @@ import os
 import config
 
 if not os.path.isdir('./data'):
-	os.makedirs('./data')
+    os.makedirs('./data')
 
 response = requests.get('http://eventlist.mk/events')
 
 if response.status_code != 200:
-	print "Something bad happened. :/"
-	exit(1)
+    print "Something bad happened. :/"
+    exit(1)
 
 api = twitter.Api(
-	consumer_key=config.CONSUMER_KEY,
-	consumer_secret=config.CONSUMER_KEY_SECRET,
-	access_token_key=config.ACCESS_TOKEN,
-	access_token_secret=config.ACCESS_TOKEN_SECRET
+    consumer_key=config.CONSUMER_KEY,
+    consumer_secret=config.CONSUMER_KEY_SECRET,
+    access_token_key=config.ACCESS_TOKEN,
+    access_token_secret=config.ACCESS_TOKEN_SECRET
 )
 
 old_events = json.loads(open('./data/old.json', 'r').read())
@@ -32,22 +32,22 @@ new_events = json.loads(response.content)
 to_tweet = []
 
 for event in new_events:
-	if event['id'] not in old_event_ids:
-		print 'adding event'
-		to_tweet.append(event)
+    if event['id'] not in old_event_ids:
+        print 'adding event'
+        to_tweet.append(event)
 
 if False:
-	for event in to_tweet:
-		print event
+    for event in to_tweet:
+        print event
 
-		name = event['name']
-		link = 'https://facebook.com/' + event['id']
+        name = event['name']
+        link = 'https://facebook.com/' + event['id']
 
-		if len(name) > 100:
-			name = name[:97] + '...'
-		
-		api.PostUpdate('TST ' + name + ' ' + link)
+        if len(name) > 100:
+            name = name[:97] + '...'
+        
+        api.PostUpdate('TST ' + name + ' ' + link)
 
-		break
+        break
 
 open('./data/old.json', 'w').write(json.dumps(new_events))
